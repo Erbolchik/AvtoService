389275128c2009ae5b2d0789e3b1f74f72b54981
erbol.daurenyli@gmail.com <erbol.daurenyli@gmail.com>
2021-04-13 23:49:17 +0600

    init commit

---
+++ AvtoService/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvtoService.Controllers
+{
+    public class LoginController
+    {
+    }
+}
