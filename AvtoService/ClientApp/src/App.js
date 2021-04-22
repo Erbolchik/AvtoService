22145b1d101dbe287a67bf21e36dfbec72035f26
erbol.daurenyli@gmail.com <erbol.daurenyli@gmail.com>
2021-04-22 23:42:12 +0600

    some fix

--- AvtoService/ClientApp/src/App.js
+++ AvtoService/ClientApp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
@@ -7,19 +7,34 @@ import { Counter } from './components/Counter';
 import Navbar from './pages/Menu';
 // import './custom.css';
 import Login from './pages/Login';
+import { BrowserRouter } from 'react-router-dom';
+import { Menu } from 'antd';
+import { PieChartOutlined, DesktopOutlined, ContainerOutlined } from '@ant-design/icons';
 
-export default class App extends Component {
-  static displayName = App.name;
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Menu defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} mode="inline" theme="dark">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Личный кабинет
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Главная
+          </Menu.Item>
+          <Menu.Item key="3" icon={<ContainerOutlined />}>
+            Сотрудники
+          </Menu.Item>
+          <Menu.Item key="3" icon={<ContainerOutlined />}>
+            Сотрудники
+          </Menu.Item>
+          <Menu.Item key="3" icon={<ContainerOutlined />}>
+            Сотрудники
+          </Menu.Item>
+        </Menu>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-  render() {
-    return (
-      <Layout>
-        <Navbar></Navbar>
-        <Route exact path="/" component={Home} />
-        <Route path="/counter" component={Counter} />
-        <Route path="/fetch-data" component={FetchData} />
-        <Route path="/login" component={Login} />
-      </Layout>
-    );
-  }
-}
+export default App;
